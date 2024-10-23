1494034c837d3fa8ba231507311a3b0647eba6f6
Alican19 <151394182+Alican19@users.noreply.github.com>
2024-10-23 14:18:12 +0000

    neuer port

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
   res.send('Hello, this is Azure Web App running a Node.js application!');
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
