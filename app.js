bcf3ec7964b42e3c1e09f859086df8936cbb2683
Alican19 <alsoit00@hs-esslingen.de>
2024-10-23 16:43:22 +0200

    erster anlauf

--- app.js
+++ app.js
@@ -2,9 +2,20 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Statische HTML-Datei ausliefern
+// Simulierte Sensordaten
+let sensorData = { temperature: 22.5, humidity: 60 };
+
+// Middleware zum Bereitstellen statischer Dateien (z.B. CSS)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route zum Abrufen der aktuellen Sensordaten (z.B. vom Raspberry Pi)
+app.get('/data', (req, res) => {
+  res.json(sensorData);  // Hier würdest du die tatsächlichen Daten vom Raspberry Pi senden
+});
+
+// Route zum Bereitstellen der Monitoring-Webseite
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 const port = process.env.PORT || 3000;
